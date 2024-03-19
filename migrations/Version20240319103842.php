aa02d11c60c26f1978918ede690af45959460167
GENIN <bgenin@beaupeyrat.fr>
2024-03-19 11:56:48 +0100

    entity anime

--- migrations/Version20240319103842.php
+++ migrations/Version20240319103842.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240318185800 extends AbstractMigration
+final class Version20240319103842 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,15 @@ final class Version20240318185800 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified BOOLEAN NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+        $this->addSql('CREATE SEQUENCE anime_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE anime (id INT NOT NULL, title VARCHAR(255) NOT NULL, episode INT NOT NULL, genre VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
-        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP SEQUENCE anime_id_seq CASCADE');
+        $this->addSql('DROP TABLE anime');
     }
 }
